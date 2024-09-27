676fde9fccfdcd0a37d9df6ade26348443f1497b
acrson <cstell@cub.uca.edu>
2024-09-26 23:13:40 -0500

    Create algorithmsProject2.py

---
+++ algorithmsProject2.py
@@ -0,0 +1,18 @@
+"""
+Algorithms Project 2
+
+TO-DO:
+    - Implement bubble sort
+    - Implement merge sort
+    - Implement quick sort
+    - Implement a 4th sort of our choosing
+    - Premake arrays before testing for best, avg, worst cases depending on array size n
+    - Document how long best, avg, worst case took for each sort, with array size n = 100, 1000, 10000
+    - Make some graphs or other figures showing differences visually
+
+Also, maybe we should implement front end / main last this time? I think we had
+a good idea trying to do it first last time but it bit us in the booties a little.
+We could discuss this in greater detail though
+"""
+
+# This will eventually contain code, I promise
