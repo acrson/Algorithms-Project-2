23c31d0a767ce3abd30612b44f27a47ea224b623
acrson <cstell@cub.uca.edu>
2024-09-27 10:44:37 -0500

    Updated To-Do
    Updated the To-Do for Fay, Carson, and Sera. Decided that the 4th sort of our choosing will be radix sort.

--- algorithmsProject2.py
+++ algorithmsProject2.py
@@ -2,12 +2,18 @@
 Algorithms Project 2
 
 TO-DO:
+    FAY:
     - Implement bubble sort
     - Implement merge sort
-    - Implement quick sort
-    - Implement a 4th sort of our choosing
+    
+    
+    CARSON:
+    - Implement radix sort
     - Premake arrays before testing for best, avg, worst cases depending on array size n
     - Document how long best, avg, worst case took for each sort, with array size n = 100, 1000, 10000
+    
+    SERA:
+    - Implement quick sort
     - Make some graphs or other figures showing differences visually
 
 Also, maybe we should implement front end / main last this time? I think we had
